2f82dcfad4d70ec7e0c13ce716d2fbbd99865b93
Berke Erdem <75441591+berkeerdem1@users.noreply.github.com>
2023-01-29 18:00:18 +0300

    Pompalı Tüfek Mermi Kodu

---
+++ shotgun_bullet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotgun_bullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float bulletSpeed;
+    public float endTime;
+    public GameObject patlama;
+    //public GameObject patlama;
+
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * bulletSpeed;
+        Destroy(gameObject, endTime);
+
+        GameObject sil = Instantiate(patlama, transform.position, transform.rotation);
+        Destroy(sil, 0.100f);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            collision.GetComponent<enemy_move>().TakeDamage(20);
+
+
+
+            Destroy(gameObject);
+            //Debug.Log("değdi");
+        }
+
+
+
+    }
+
+}
